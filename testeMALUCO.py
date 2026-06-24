f9b68a4b6f5b23675c1bfe05ff2bc051ba98b308
Thales-Vialta <thalesvialta12@gmail.com>
2026-06-24 11:25:34 -0300

    'delete comments'

--- testeMALUCO.py
+++ testeMALUCO.py
@@ -12,7 +12,7 @@ from services.salaService import salaService
 repo_Sala = salaRepository()
 servSala = salaService(repo_Sala)
 
-'''print(servSala.listarSalas(),'\n')
+print(servSala.listarSalas(),'\n')
 
 print(servSala.buscarSalas('Auditório Principal'),'\n')
 
@@ -22,74 +22,48 @@ print(servSala.cadastrarSalas(100,'Sala dos mano', 'A113 | Bloco B | 2°Andar'),
 
 print(servSala.editarSalas('Sala das Mana','EnderecoSala','A67'),'\n')
 
-print(servSala.removerSalas('Sala das Mana'),'\n')'''
-'''repo_Cargo = CargoRepository()
+print(servSala.removerSalas('Sala das Mana'),'\n')
+repo_Cargo = CargoRepository()
 servCargo = CargoService(repo_Cargo)
 repo_Horario = HorarioRepository()
-servHora = horarioService(repo_Horario)'''
+servHora = horarioService(repo_Horario)
 
 
 
-'''repo_Usuario = UsuarioRepository()
-serv_Usuario = usuarioService(repo_Usuario,servCargo)'''
+repo_Usuario = UsuarioRepository()
+serv_Usuario = usuarioService(repo_Usuario,servCargo)
 
-'''servHora.listarHorarios()'''
+servHora.listarHorarios()
 
-"""cargoAleatorio = 'Professor'"""
+cargoAleatorio = 'Professor'
 
-'''userCap = serv_Usuario.capturaUsuarios()
-print(userCap)'''
+userCap = serv_Usuario.capturaUsuarios()
+print(userCap)
 
-#search_user = serv_Usuario.buscaUsuario('João de Oliveira Stekel')
-#print(search_user)
+search_user = serv_Usuario.buscaUsuario('João de Oliveira Stekel')
+print(search_user)
 
-'''Validar = serv_Usuario.validarUsuario('João de Oliveira Stekel')
-print(Validar)'''
+Validar = serv_Usuario.validarUsuario('João de Oliveira Stekel')
+print(Validar)
 
-'''atualizar = serv_Usuario.atualizaUsuario('Joana de Oliveira Stekel','ID_Cargo','2')
-print(atualizar)'''
+atualizar = serv_Usuario.atualizaUsuario('Joana de Oliveira Stekel','ID_Cargo','2')
+print(atualizar)
 
-"""eliminar_joao = serv_Usuario.removerUsuario('João')
-print(eliminar_joao)"""
+eliminar_joao = serv_Usuario.removerUsuario('João')
+print(eliminar_joao)
 
 
-"""
-cargos = serv.capturarCargos()
-print(cargos)"""
 
-"""idCargo = serv.capturarIdCargo(cargoAleatorio)
+cargos = servCargo.capturarCargos()
+print(cargos)
+
+idCargo = servCargo.capturarIdCargo(cargoAleatorio)
 print(idCargo)
 
 
 for id_usuario, nome in repo_Usuario.Usuario_Nunca_Alocou():
     print(f"{id_usuario} - {nome}")
 
-serv_Usuario = usuarioService(repo_Usuario,serv)
+serv_Usuario = usuarioService(repo_Usuario,servCargo)
 print('Teste começa aqui')
 print(serv_Usuario.cadastrarUsuario('João', 'Professor'))
-"""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
