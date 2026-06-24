f94b8b6eb04e8c9ddb782d718eb6bed71b28de22
Aron G. <aron.g@aluno.ifsp.edu.br>
2026-06-24 09:16:03 -0300

    Update aparelhos.py

--- models/aparelhos.py
+++ models/aparelhos.py
@@ -6,17 +6,6 @@ class Aparelhos:
         self.__statusAparelho = statusAparelho
         self.__idModelo = idModelo
 
-    @property
-    def serial(self): 
-        return self.__serial
-    @property
-    def statusAparelho(self): 
-            return self.__statusAparelho
-    @property
-    def idModelo(self): 
-            return self.__idModelo
-    
-    #essa func. serve pra quando der print em um obj ele imprimir os valores dos atributos e não o caminho do obj
     def __str__(self):
         return f"Aparelho [ID: {self.__idAparelho} | Serial: {self.__serial} | Status: {self.__statusAparelho} | Modelo: {self.__idModelo}]"
     
