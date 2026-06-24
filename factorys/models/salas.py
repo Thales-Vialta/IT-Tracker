dc87d3cd2489d58ef792dd1e8213480e055a7b2e
Thales-Vialta <thalesvialta12@gmail.com>
2026-06-24 08:59:48 -0300

    '0.1.5 fix: Fixing doubled folders'

--- factorys/models/salas.py
+++
@@ -1,8 +0,0 @@
-class Sala:
-    def __init__(self, NomeSala: str, EnderecoSala: str):
-        self.idSala = None
-        self.NomeSala = NomeSala
-        self.EnderecoSala = EnderecoSala
-
-    def __str__(self):
-        return f"Sala [ID: {self.idSala} | Nome: {self.NomeSala} | Endereço: {self.EnderecoSala}]"
--- factorys/views/visualReservasView.py
+++
@@ -1,5 +0,0 @@
-from menuPrincipalView import limpar_tela() """não lembro como faz"""
-
-class visualizarReservasView:
-    def visualizarReservas(self):
-        self.limpar_tela() """não lembro como faz"""
