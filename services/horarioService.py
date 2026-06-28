ab7021892a72cc0b0f28aadbb33d20b492edf187
Grazielly Galeano <graziellygaleano@gmail.com>
2026-06-28 17:04:13 -0300

    horarioService em desenvolvimento

--- services/horarioService.py
+++ services/horarioService.py
@@ -1,4 +1,3 @@
-from models.horario import Horario
 from repositories.horarioRepository import horarioRepo
 
 from views.limparTela import limpar_tela
@@ -12,9 +11,9 @@ class horarioService:
     def listarHorarios(self):
         limpar_tela()
 
-        hora = self.horario_repo.Listar_Horario()
+        hora = self.horario_repo.Mostrar_Horario()
 
-        resultado = f"{CORES['AZUL']}{CORES['NEGRITO']}---- LISTA DE HORÁRIOS ----\n\n{CORES['RESET']}"
+        resultado = f"{CORES['AZUL']}{CORES['NEGRITO']}---- LISTA DE HOR├üRIOS ----\n\n{CORES['RESET']}"
 
         for HoraInicio, HoraFim, Descricao in hora:
             inicio_str = str(HoraInicio)
@@ -26,5 +25,22 @@ class horarioService:
             resultado += f"{CORES['AMARELO']}{CORES['NEGRITO']}{desc_format} | {CORES['RESET']} {CORES['RESET']} {periodo}\n"
 
         return resultado
+    
+    def validarHorarioFunc(self):
+        horarios_cadastrados = self.horario_repo.Listar_Horario()
+        
+        for HoraInicio, HoraFim, Descricao in horarios_cadastrados:
+            if Descricao.strip() == 'Horario de Funcionamento':
+                return 'Deseja cadastrar um intervalo?'
+                
+        return 'Deseja cadastrar o horario de funcionamento?'
+    
+    def intervaloHora(self):
+        pass
+        
+    
+    def criarIntervaloHora(self,desc,inicio,fim):
+        self.horario_repo.Inserir_Horario(desc,inicio,fim)
+        pass
 
 horarioService = horarioService(horarioRepo)
